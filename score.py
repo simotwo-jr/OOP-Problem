938343b925d4722b7ef11df1ae892610ae9d4521
Nehemiah <nehemiahcheruiyot7069@gmail.com>
2024-03-25 11:50:03 +0300

    feat: Participants

--- score.py
+++ score.py
@@ -8,6 +8,13 @@ def create_scoreboard(participants):
     
     return scoreboard
 
+participants = [
+    {'name': "Habanero Hillary", 'chickenwings': 5, 'hamburgers': 17, 'hotdogs': 11},
+    {'name': "Big Bob", 'chickenwings': 20, 'hamburgers': 4, 'hotdogs': 11}
+]
+
+
+
 
 
 
