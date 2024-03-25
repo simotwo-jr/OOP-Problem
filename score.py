806fab185e7acfcc61bd4aff2a8b656f160408aa
Nehemiah <nehemiahcheruiyot7069@gmail.com>
2024-03-25 11:48:09 +0300

    feat: Defining a function create_scoreboard

--- score.py
+++ score.py
@@ -2,3 +2,12 @@ def calculate_score(participant):
     score = participant['chickenwings'] * 5 + participant['hamburgers'] * 3 + participant['hotdogs'] * 2
     return score
 
+def create_scoreboard(participants):
+    scoreboard = [{'name': participant['name'], 'score': calculate_score(participant)} for participant in participants]
+    scoreboard.sort(key=lambda x: (-x['score'], x['name']))
+    
+    return scoreboard
+
+
+
+
